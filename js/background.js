3d67e26fb999c0540c5e7b292bdc38129909a1e8
Thomas Donahue <tom@klunkmillan.com>
2016-03-01 22:37:29 -0500

    Added in the start of content scripts for the etix pages

--- js/background.js
+++ js/background.js
@@ -1,5 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
   chrome.tabs.executeScript({
     code: 'document.body.style.backgroundColor="red"'
   });
