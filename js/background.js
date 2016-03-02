fe8370732f9395bc50800f920c7fe21615978422
Thomas Donahue <tom@klunkmillan.com>
2016-03-02 00:31:40 -0500

    Removing background things that don't work

--- js/background.js
+++ js/background.js
@@ -1,6 +1,1 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
 // http://stackoverflow.com/questions/12065029/chrome-redirect-extension
--- js/content_script.js
+++ js/content_script.js
@@ -1,1 +1,2 @@
-alert('hello');
+alert('Why am I getting these alerts on ETix?');
+alert('And why is the background pink?');
